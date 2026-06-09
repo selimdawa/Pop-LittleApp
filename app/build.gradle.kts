29d7d0c0bbf379f72b4d20dcbedbcbdc9bf9c7b8
selimdawa <selimdawa@gmail.com>
2026-06-09 03:49:09 +0300

    SDK 37 / Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,13 @@ plugins {
 android {
     namespace = "com.littleapp.pop"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.littleapp.pop"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
