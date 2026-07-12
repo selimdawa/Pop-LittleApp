ebb9db712a3d634f336b2549eef1570d1c119e8e
selimdawa <selimdawa@gmail.com>
2026-07-13 00:17:42 +0300

    statusBarColor, Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.dagger.hilt.android)
     alias(libs.plugins.navigation.safeargs.kotlin)
-    alias(libs.plugins.ksp.processor)
-    alias(libs.plugins.daggerHiltAndroid)
 }
 
 android {
@@ -57,18 +57,21 @@ dependencies {
     implementation(libs.coil.kt)                         //Coil
     //Lifecycle
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    //Kotlin Coroutines
+    //Coroutines
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
+    //Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
     //Navigation
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    //Dagger-Hilt
+    //Dagger - Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.fragment)
     //Other's
-    implementation(libs.timber)                          //Timber Log
     implementation(libs.jsoup)                           //Jsoup
+    implementation(libs.timber)                          //Timber Log
 }
